2c7d4fbf2640da5b516f2b04e7fa99642630f838
masayuki.toyota <utoyo272@gmail.com>
2021-05-19 19:32:41 +0000

    last commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,20 +4,51 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+      @task = Task.new(task_params)
+      
+      if @task.save
+        flash[:success] = 'Taskが正常に入力されました'
+        redirect_to @task
+      else
+        flash.now[:danger] = 'Taskが入力されませんでした'
+        render :new
+      end
   end
 
   def edit
+      @task = Task.find(params[:id])
   end
 
   def update
+      @task = Task.find(params[:id])
+
+      if @task.update(task_params)
+        flash[:success] = 'Task は正常に更新されました'
+        redirect_to @task
+      else
+        flash.now[:danger] = 'Task は更新されませんでした'
+        render :edit
+      end
   end
 
   def destroy
+      @task = Task.find(params[:id])
+      @message.destroy
+
+      flash[:success] = 'Task は正常に削除されました'
+      redirect_to tasks_url
+    
+  end
+  
+  def task_params
+      params.require(:task).permit(:content)
   end
 end
