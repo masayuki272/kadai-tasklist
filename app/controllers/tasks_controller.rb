85813095db504fae3fe6c20fb83dab4ffd6cb545
masayuki.toyota <utoyo272@gmail.com>
2021-06-21 07:14:03 +0000

    for heroku8

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :new]
+  before_action :require_user_logged_in, only: [:index, :new, :show]
   before_action :correct_user, only: [:show, :destroy, :edit]
 
   def index
